ea872823fd6c04080e1b9293c4d56b75ac6da3d1
defDalila <dalila.fismed@gmail.com>
2024-11-18 22:25:35 -0300

    feat(validator): implement initial IPv4 address validation logic

--- Ipv4Validator/Helpers/DataValidate.cs
+++ Ipv4Validator/Helpers/DataValidate.cs
@@ -5,8 +5,72 @@ using System.Threading.Tasks;
 
 namespace Ipv4Validator.Helpers
 {
-    public class DataValidate
-    {
-        
-    }
+	public static class DataValidate
+	{
+		public static void ValidateIp(string ipAdress)
+		{
+			if (IsDotsValid(ipAdress))
+			{
+				Console.WriteLine($"{ipAdress} é válido");
+			}
+			else
+			{
+				Console.WriteLine($"{ipAdress} é inválido");
+			}
+		}
+		
+		private static bool IsDotsValid(string ipAddress)
+		{
+						
+			var isValid = ipAddress.Count(c => c == '.').Equals(3);
+			
+			if (isValid) return IsOctetsLengthValid(ipAddress.Split('.'));
+			
+			return false;
+		}
+		
+		
+		private static bool IsOctetsLengthValid(string[] octets)
+		{
+			
+			var isValid = octets.All(octet => octet.Length > 0 && octet.Length < 4);
+			
+			if (isValid) return IsOctetsContentsValid(octets);
+			
+			return false;
+		}
+		
+		private static bool IsOctetsContentsValid(string[] octets)
+		{
+						
+			var isValid = octets.SelectMany(o => o.ToArray()).All(e => int.TryParse(e.ToString(), out _));
+			
+			if (isValid) return IsOctetStartValid(octets);
+			
+			return false;
+		}
+		
+		private static bool IsOctetStartValid(string[] octets)
+		{
+						
+			var isValid = octets.Where(o => o.Length > 1).Count(e => e.StartsWith("0")).Equals(0);
+			
+			if (isValid) return IsOctetRangeValid(octets);
+			
+			
+			return false;
+		}
+		
+		private static bool IsOctetRangeValid(string[] octets)
+		{
+			
+			var isValid = octets.All(o => byte.TryParse(o, out _));
+			
+			if (isValid) return true;
+			
+			return false;
+		}
+		
+		
+	}
 }
--- Ipv4Validator/Program.cs
+++ Ipv4Validator/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ipv4Validator.Helpers;
+using Ipv4Validator.Utils;
+
+Console.Clear();
+
+string[] ipValidos = DataProvider.GetValidIpSamples();
+string[] ipInvalidos = DataProvider.GetInvalidIpSamples();
+
+foreach (var ip in ipInvalidos)
+{
+	DataValidate.ValidateIp(ip);
+}
+
+Console.WriteLine("---------------------\n");
+
+foreach (var ip in ipValidos)
+{
+	DataValidate.ValidateIp(ip);
+}
+
+Console.WriteLine("----------------\n");
+
+string[] ipAddresses = DataProvider.GetIpSamples();
+foreach (var ip in ipAddresses)
+{
+	DataValidate.ValidateIp(ip);
+}
+
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
+
+Console.Clear();
